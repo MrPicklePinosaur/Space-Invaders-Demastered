4ea86938a963b256479b607ac330f524f17f4f5c
Daniel Liu <mr.picklepinosaur@gmail.com>
2019-02-01 17:28:04 -0500

    worked on body-sprite syncing

--- core/src/com/mygdx/game/Entity.java
+++ core/src/com/mygdx/game/Entity.java
@@ -4,6 +4,7 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
 
 //     'An entity is anything that has position and texture'
@@ -12,46 +13,43 @@ public abstract class Entity {
     //NOTE: I dont think you need to have protected variables in an abstract class, look into further later
     protected Sprite sprite;
     protected Body body;
-    protected float posX;
-    protected float posY;
     protected float rotation;
 
     public Entity(Texture texture) { //NOTE: superclass never uses world
         this.sprite = new Sprite(texture);
-        this.posX = 0;
-        this.posY = 0;
-        this.rotation = 0;
     }
 
     public Body create(FixtureDef fdef) { ///takes in a fixture definition and creates a body
         //Create the body
         BodyDef bdef = new BodyDef();
         bdef.type = BodyDef.BodyType.DynamicBody;
-        bdef.position.set(this.posX/Global.PPM,this.posY/Global.PPM);
+        bdef.position.set(this.getX()/Global.PPM,this.getY()/Global.PPM);
         Body new_body = Global.world.createBody(bdef);
         new_body.createFixture(fdef); //DONT FORGET TO DISPOSE OF fdef
 
         return new_body;
     }
-    public void update() { //Sync sprite with body
+    public void update() { //Sync sprite with body (sprite follows body)
+        //Sync position
+        Vector2 bodyPos = this.body.getPosition(); //get body position
+        this.sprite.setPosition(bodyPos.x*Global.PPM,bodyPos.y*Global.PPM);
+
+        //Sync rotation
 
     }
 
     public abstract void destroy();
 
     //Getters
-    public float getX() { return this.posX; }
-    public float getY() { return this.posY; }
-    public float getRotation() { return this.rotation; } //make decision on rotation being in radians or degrees
+    public float getX() { return this.sprite.getX(); }
+    public float getY() { return this.sprite.getY(); }
+    public float getRotation() { return this.sprite.getRotation(); } //make decision on rotation being in radians or degrees
 
     //Setters
     public void init(float posX, float posY, float rotation) { //used to place an entity in a specific orientation in the world
-        this.setX(posX);
-        this.setY(posY);
-        this.setRotation(rotation);
+        this.sprite.setX(posX);
+        this.sprite.setY(posY);
+        this.sprite.setRotation(rotation);
     }
-    //These methods may be one time use, so possibly merge with init()
-    public void setX(float posX) { this.posX = posX; }
-    public void setY(float posY) { this.posY = posY;}
-    public void setRotation(float rotation) {this.rotation = rotation; }
+
 }
